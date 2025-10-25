7a856ad8700d38756da70bafbf2934752889dc93
chanbinme <gksmfcksqls@gmail.com>
2025-10-26 00:46:22 +0900

    feat: 인증 헤더와 날짜 형식 상수 정의

---
+++ src/main/kotlin/com/kio/qit/constant/Constant.kt
@@ -0,0 +1,10 @@
+package com.kio.qit.constant
+
+object Constant {
+    // Authorization header
+    const val AUTHORIZATION_HEADER = "Authorization"
+    const val BEARER_TYPE = "Bearer"
+
+    // Datetime format
+    const val DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss"
+}
