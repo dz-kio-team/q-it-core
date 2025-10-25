74d2582d3a92a22eb632196e7bb18fd18c6adaee
chanbinme <gksmfcksqls@gmail.com>
2025-10-26 00:47:43 +0900

    chore: 스프링 부트 플러그인 의존성 관리 설정 및 추가 라이브러리 의존성 정의
    - BOM_COORDINATES은 Spring Boot 플러그인 없이도 프레임워크의 의존성 관리를 할 수 있는 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.5.6" apply false
     id("io.spring.dependency-management") version "1.1.7"
-    `java-library`
 }
 
 group = "com.kio"
@@ -20,10 +19,24 @@ repositories {
     mavenCentral()
 }
 
+// Spring Boot Plugin 없이도 스프링 프레임워크의 의존성 관리를 사용할 수 있도록 설정
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib"))    // Kotlin 표준 라이브러리
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+
+    // Kotlin Logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
+
+    // Jackson
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
 
